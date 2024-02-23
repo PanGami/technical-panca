d4d51c2da2a0cdaebb4e381877514c88bc05e369
PanGami <pancanugrahawicaksana@gmail.com>
2024-02-23 13:45:43 +0700

    Add middleware at cart routes - User have to be logged in

--- routes/cart.route.go
+++ routes/cart.route.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/PanGami/technical-panca/controllers"
+	"github.com/PanGami/technical-panca/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func CartRoutes(router fiber.Router) {
 	cart := router.Group("/cart")
-	cart.Post("/add", controllers.AddToCart)
-	cart.Get("/items", controllers.GetCartItems)
-	cart.Delete("/delete/:id", controllers.DeleteCartItem)
-	cart.Post("/checkout", controllers.Checkout)
+	cart.Post("/add", middleware.DeserializeUser, controllers.AddToCart)
+	cart.Get("/items", middleware.DeserializeUser, controllers.GetCartItems)
+	cart.Delete("/delete/:id", middleware.DeserializeUser, controllers.DeleteCartItem)
+	cart.Post("/checkout", middleware.DeserializeUser, controllers.Checkout)
 }
